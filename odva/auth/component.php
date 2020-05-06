891b2faa88b9830a14fb46e11dbb24729b85b5e9
aczamaz <aczamaz@gmail.com>
2020-05-06 14:48:25 +0300

    description auth component done

--- odva/auth/component.php
+++ odva/auth/component.php
@@ -1,13 +1,17 @@
 <?php
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 CModule::IncludeModule("iblock");
-
+//проверка на то авторизован пользователь или нет, если нет то компоннт не подгружается
 global $USER;
 if($USER->IsAuthorized())
 	return false;
-
+//путь к файлу для авотризации/регистрации через соцсети
 $arResult['AUTH_SOCIAL_PATH'] = "$componentPath/authSocial.php";
+//путь к файлу для стандартной регистрации
 $arResult['REG_BITRIX_PATH'] = "$componentPath/regBitrix.php";
+//путь к файлу для стандартной авторизации
 $arResult['AUTH_BITRIX_PATH'] = "$componentPath/authBitrix.php";
+//путь к файлу для смены пароля и отправки его на почту
 $arResult['SEND_PASSWORD_BITRIX_PATH'] = "$componentPath/sendPassword.php";
+//вызов указаного в подклчючении компонента шаблона
 $this->IncludeComponentTemplate();
