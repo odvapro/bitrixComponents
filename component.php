ba9cf4fe1798e39b9e7ed4baff4bad6c8985fd4d
Diana Tuaeva <diatuaeva@gmail.com>
2021-03-16 11:33:18 +0300

    fix

--- component.php
+++ component.php
@@ -1,33 +1,8 @@
 <?php
 
 $arResult = [
-	'COUNT' => count(\Odva\Helpers\Favorites::get()),
-	'ELEMENTS' => \Odva\Helpers\Favorites::get(),
-	'URL'   => ($USER->IsAuthorized() ? '/page/favorites/' : '#login')
+	'COUNT' => count(\Odva\Module\Favorites::get()),
+	'ELEMENTS' => \Odva\Module\Favorites::get(),
 ];
 
 $this->IncludeComponentTemplate();
-
-$templatePath = $this->getTemplate()->GetFolder();
-
-$jsModulePath = "{$templatePath}/script.js";
-
-$jsModuleName = "{$this->getName()}_{$this->getTemplate()->getName()}";
-$jsModuleName = preg_replace("/[^a-z0-9_]/i", '_', $jsModuleName);
-
-if(
-	!CJSCore::IsExtRegistered($jsModuleName)
-	&&
-	file_exists($jsModulePath)
-)
-{
-	CJSCore::RegisterExt(
-		$jsModuleName,
-		[
-			'js' => $jsModulePath,
-			'rel' => ['odva']
-		]
-	);
-
-	CUtil::InitJSCore([$jsModuleName]);
-}
