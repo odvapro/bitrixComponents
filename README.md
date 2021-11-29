6ad9b6ee9a89bb883afb742baa588791251c4279
z-z <bazzaurik@gmail.com>
2021-11-29 17:30:07 +0300

    refactoring, add new params, remove old params and templates

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # Список разделов
 
-- ___filter___ - параметр **arFilter** для метода ```CIBlockElement::GetList```
-- ___count___ - количество доставаемых секций (по умолчанию count = 10)
+- ___sort___        - параметр **arOrder** для метода ```CIBlockSection::GetList```
+- ___filter___      - параметр **arFilter** для метода ```CIBlockSection::GetList```
+- ___element_cnt___ - параметр **bIncCnt** для метода ```CIBlockSection::GetList```
+- ___select___      - параметр **Select** для метода ```CIBlockSection::GetList```
+- ___count___       - количество доставаемых секций (по умолчанию достаются все)
 
 пример использования
 ```php
 $APPLICATION->IncludeComponent(
-	'odva:sections',// odva - простараства имен в котором находится компонент, sections - имя компонента
-	'sections',// имя шаблона компонента
+	'odva:sections',
+	'sections',
 	[
-		'filter' => [
-			'IBLOCK_ID' => 5,// id - инфоблока
-		],
-		'count' => 100 // количество доставаемых секций
+		'sort'        => ['SORT' => 'ASC'],
+		'filter'      => [],
+		'element_cnt' => true,
+		'select'      => [],
+		'count'       => 100,
 	]
 );
 ```
--- class.php
+++ class.php
@@ -1,45 +1,50 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+<?php
 
-class Sections extends CBitrixComponent
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+class OdvaSections extends CBitrixComponent
 {
+	public function onPrepareComponentParams($params)
+	{
+		$params = parent::onPrepareComponentParams($params);
+
+		if(!is_array($params['filter']) || empty($params['filter']))
+			$params['filter'] = [];
+
+		if(!is_array($params['sort']) || empty($params['sort']))
+			$params['sort'] = ['SORT' => 'ASC'];
+
+		$params['element_cnt'] = !empty($params['elements_count']);
+
+		if(!is_array($params['select']) || empty($params['select']))
+			$params['select'] = [];
+
+		if(empty($params['count']) || intval($params['count']) <= 0)
+			$params['nav_params'] = ['nTopCount' => false];
+		else
+			$params['nav_params'] = ['nTopCount' => intval($params['count'])];
+
+		return $params;
+	}
+
 	public function executeComponent()
 	{
 		if(!\Bitrix\Main\Loader::includeModule("iblock"))
 			return;
 
-		$this->getSections($this->arParams['filter'], $this->arParams['sort'], $this->arParams["count"]);
+		$res = CIBlockSection::GetList(
+			$this->arParams['sort'],
+			$this->arParams['filter'],
+			$this->arParams['element_cnt'],
+			$this->arParams['select'],
+			$this->arParams['nav_params']
+		);
+
+		$this->arResult['SECTIONS'] = [];
+
+		while($section = $res->Fetch())
+			$this->arResult['SECTIONS'][] = $section;
 
 		$this->IncludeComponentTemplate();
 	}
-	public function getSections($filter, $sort, $count = 10)
-	{
-		$arSelect = [];
-		$sections = [];
-
-		if(!is_array($sort) || empty($sort))
-			$sort = [];
-
-		$res = CIBlockSection::GetList($sort, $filter, true, $arSelect,["nPageSize" => $count]);
-		while($ob = $res->GetNextElement())
-		{
-			$arFields            = $ob->GetFields();
-			$arFields["PICTURE"] = CFile::ResizeImageGet($arFields["PICTURE"], ['width'=>639, 'height'=>551], BX_RESIZE_IMAGE_PROPORTIONAL, true);
-			$arFields['ELEMENTS_COUNT'] = $this->formatProductsCount($arFields['ELEMENT_CNT']);
-			$sections[]          = $arFields;
-		}
-		$this->arResult["SECTIONS"] = $sections;
-	}
-
-	public function formatProductsCount($count)
-	{
-		if($count == 1)
-			return "{$count} продукт";
-		elseif($count > 1 and $count < 5)
-			return "{$count} продукта";
-		elseif($count > 4)
-			return "{$count} продуктов";
-		else
-			return "Нет продуктов";
-	}
-
 }
